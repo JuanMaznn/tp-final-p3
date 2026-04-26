2de3a33b34fd8eae6fdfa0f73b5ad38ab065cdba
mterrera29 <matiasterrera29@gmail.com>
2026-04-26 18:49:38 -0300

    actualizacion clases 26/4

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const express = require('express')
+import express from 'express';
+import { pool } from './db/conexion.js';
+import { testConexion } from './db/test.js';
+
 const app = express();
+
+await testConexion();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.status(200)
-    res.send({'estado': 'ok', 'ms':'api ok'})
-})
+  res.status(200);
+  res.send({ estado: 'ok', ms: 'api ok' });
+});
 
-app.post('especialidades/', (req, res) => {
+app.get('/especialidades', async (req, res) => {
+  try {
+    const sql = 'SELECT * FROM especialidades WHERE activo = 1';
 
-})
-// cargar el archivo 
+    const [especialidades, fields] = await pool.query(sql);
+
+    res.status(200).send({ estado: 'ok', especialidades: especialidades });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/especialidades/:id_especialidades', async (req, res) => {
+  try {
+    const ccc = req.params.id_especialidades;
+    // const sql = `SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ${id_especialidades}`;
+
+    const sql =
+      'SELECT * FROM especialidades WHERE activo = 1 AND id_especialidad = ?';
+
+    // const [especialidades, fields] = await pool.query(sql);
+    const [especialidades, fields] = await pool.execute(sql, [ccc]);
+
+    // console.log(fields);
+
+    res.status(200).send({ estado: 'ok', especialidades: especialidades });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 process.loadEnvFile();
-const port = process.env.PUERTO
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const PUERTO = process.env.PUERTO;
 
-console.log("ab")
+app.listen(PUERTO || 3000, () => {
+  console.log(`Servidor iniciado en puerto ${PUERTO}`);
+});
